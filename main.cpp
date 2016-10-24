677264d7a83338daaebac3fec78b851a9b92a727
Andrey Mihadyuk <mihadyuk@ntlab.com>
2016-10-24 10:08:18 +0300

    main: Added static large array example.

--- main.cpp
+++ main.cpp
@@ -10,9 +10,15 @@ int f(int *a) {
     return (&b > a);
 }
 
+static uint8_t test_large_array[1024 * 1024 * 128L];
+
 int main(int argc, char *argv[]) {
 
-  printf(f(0)? "true \n" : "false \n");
+  for (uint64_t i = 0; i < sizeof(test_large_array) / sizeof(test_large_array[0]); i++) {
+      test_large_array[i] = 0;
+      printf("%d", test_large_array[i]);
+  }
+  //printf(f(0)? "true \n" : "false \n");
 
   //std::cout << "\n template makefile project \n";
   return 0;
