61cda7f3bb6f5344631d572c09c0e7f7dc633eae
Andrey Mihadyuk <mihadyuk@ntlab.com>
2016-10-24 12:09:58 +0300

    main: Minor changes.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <iostream>
 #include <stdio.h>
+#include <stdint.h>
 
 int f(int *a) {
     int b;
@@ -10,12 +11,12 @@ int f(int *a) {
     return (&b > a);
 }
 
-static uint8_t test_large_array[1024 * 1024 * 128L];
+static uint8_t test_large_array[1024 * 1024 * 128L] = {1, 2, 3, 4};
 
 int main(int argc, char *argv[]) {
 
-  for (uint64_t i = 0; i < sizeof(test_large_array) / sizeof(test_large_array[0]); i++) {
-      test_large_array[i] = 0;
+  for (uint32_t i = 0; i < sizeof(test_large_array) / sizeof(test_large_array[0]); i++) {
+      //test_large_array[i] = 0;
       printf("%d", test_large_array[i]);
   }
   //printf(f(0)? "true \n" : "false \n");
