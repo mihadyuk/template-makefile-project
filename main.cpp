6f6ace35dd48fbbf2e2ea26e90c42532a0a79c0b
Andrey Mihadyuk <mihadyuk@ntlab.com>
2016-11-05 21:43:41 +0300

    main: tests for string utils.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,25 @@
 #include <assert.h>
 #include <iostream>
+#include "stringUtils.h"
 
+using namespace utils;
+using namespace std;
+
+static void printDirName(const char *dir) {
+  printf("src: %s, dst: %s \n", dir, utils::dirname(dir).c_str());
+}
 
 int main(int argc, char *argv[]) {
 
-  std::cout << "template makefile project \r\n";
+  cout << "template makefile project \r\n";
+
+  printDirName("/tmp/test-dir/settings.ini");
+  printDirName("/settings.ini");
+  printDirName("settings.ini");
+  printDirName("/");
+  printDirName("");
+  //printDirName(nullptr);
+
   return 0;
 }
 
