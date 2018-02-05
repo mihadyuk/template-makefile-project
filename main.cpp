d9e6938edfa1d76eba2d8c03d745f2f004543c87
Andrey Mihadyuk <maoi.by@gmail.com>
2018-02-05 22:53:35 +0300

    Fix conversion.

--- main.cpp
+++ main.cpp
@@ -10,17 +10,19 @@ uint8_t bin2bcd(uint8_t bin) {
     buf.m_buf[1] = 0;
     buf.m_buf[0] = bin;
     printf("val: %.4X, 0: %d, 1: %d\n", buf.m_val, buf.m_buf[0], buf.m_buf[1]);
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 7; i++) {
         
         buf.m_val <<= 1;
-        if ((buf.m_buf[1] & 0xF) > 0x9) {
-          buf.m_buf[1] += 0x06;
+        if (((buf.m_buf[1] + 0x03) & 0x08) == 0x08) {
+          buf.m_buf[1] += 0x03;
         }
-        if ((buf.m_buf[1] & 0xF0) > 0x90) {
-          buf.m_buf[1] += 0x60;
+        if (((buf.m_buf[1] + 0x30) & 0x80) == 0x80) {
+          buf.m_buf[1] += 0x30;
         }
-        printf("val: %.4X, 0: %d, 1: %d\n", buf.m_val, buf.m_buf[0], buf.m_buf[1]);
+        printf("val: %.4X, 0: %.2X, 1: %.2X\n", buf.m_val, buf.m_buf[0], buf.m_buf[1]);
+        
     }
+    buf.m_val <<= 1;
     return buf.m_buf[1];
 }
 
