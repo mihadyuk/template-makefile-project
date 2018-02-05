f8fccdd3a89ca149486f928f29ab370806251748
Andrey Mihadyuk <maoi.by@gmail.com>
2018-02-05 21:28:24 +0300

    Add function to convert bin to bcd. It does not work and has to be fixed.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <assert.h>
 #include <iostream>
 
+uint8_t bin2bcd(uint8_t bin) {
+    union {
+        uint16_t m_val;
+        uint8_t m_buf[2];
+    } buf;
+    
+    buf.m_buf[1] = 0;
+    buf.m_buf[0] = bin;
+    printf("val: %.4X, 0: %d, 1: %d\n", buf.m_val, buf.m_buf[0], buf.m_buf[1]);
+    for (int i = 0; i < 8; i++) {
+        
+        buf.m_val <<= 1;
+        if ((buf.m_buf[1] & 0xF) > 0x9) {
+          buf.m_buf[1] += 0x06;
+        }
+        if ((buf.m_buf[1] & 0xF0) > 0x90) {
+          buf.m_buf[1] += 0x60;
+        }
+        printf("val: %.4X, 0: %d, 1: %d\n", buf.m_val, buf.m_buf[0], buf.m_buf[1]);
+    }
+    return buf.m_buf[1];
+}
 
 int main(int argc, char *argv[]) {
 
