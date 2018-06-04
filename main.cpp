909cdd7d964c5d948bac630aa5df7ae67b5119c7
Andrey Mihadyuk <maoi.by@gmail.com>
2018-06-04 22:19:14 +0300

    Add test code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 //#include <assert.h>
 //#include <iostream>
 //#include <stdio.h>
+#include <stdint.h>
 
+struct Test {
+  int32_t _a;
+  int64_t _b;
+  int16_t _c;
+};
+
+Test test1;
+Test test2 = {4};
 
 int main(void) {
 
