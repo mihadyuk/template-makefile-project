0665bacb9a9d313b54ecadd9b7738e4c0d7b00ae
Andrey Mihadyuk <maoi.by@gmail.com>
2018-07-05 18:00:20 +0300

    Add class example

--- classes/classesMain.cpp
+++ classes/classesMain.cpp
@@ -5,11 +5,17 @@
  *      Author:
  */
 #include "classesMain.h"
-
-
+#include "classesBase.h"
+#include <stdio.h>
 
 void classesMain() {
 
+    Base a;
+    Base b;
+    Base c = a;
+    Base d(b);
+
+    b = a;
 }
 
 
