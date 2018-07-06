801a2d9895da469cd053428edee102453133da56
Andrey Mihadyuk <maoi.by@gmail.com>
2018-07-06 12:53:05 +0300

    Fix warnings

--- aggregateInit/aggregateInitMain.cpp
+++ aggregateInit/aggregateInitMain.cpp
@@ -24,11 +24,13 @@ typedef union {
 
 void aggregateInitMain() {
 
-    PlainStruct plainStruct = {.m_int32 = -1, .m_uint16 = 2};
+    //PlainStruct plainStruct = {.m_int32 = -1, .m_uint16 = 2};
+    PlainStruct plainStruct = {-1, 2};
     printf("int32: %d, uint16: %u \r\n", plainStruct.m_int32, plainStruct.m_uint16);
 
     //PlainUnion un = {.bits = {1, 2, 3}};
-    PlainUnion un = {.m_val = 0x123};
+    //PlainUnion un = {.m_val = 0x123};
+    PlainUnion un = {0x123};
     printf("bit1: %d, bit2: %d, reserved: %d \r\n", un.bits.m_bit1, un.bits.m_bit2, un.bits.m_reserved);
 }
 
--- classes/classesBase.cpp
+++ classes/classesBase.cpp
@@ -9,28 +9,28 @@
 #include <stdio.h>
 
 Base::Base() {
-    printf("Base: ctor %p \r\n", this);
+    printf("Base: ctor %p \r\n", (void *)this);
 }
 
 Base::Base(const Base &src) {
-    printf("Base: copy ctor %p = %p \r\n", this, &src);
+    printf("Base: copy ctor %p = %p \r\n", (void *)this, (void *)&src);
 }
 
 Base::Base(const Base &&src) {
-    printf("Base: move ctor %p = %p \r\n", this, &src);
+    printf("Base: move ctor %p = %p \r\n", (void *)this, (void *)&src);
 }
 
 Base::~Base() {
-    printf("Base: dtor %p \r\n", this);
+    printf("Base: dtor %p \r\n", (void *)this);
 }
 
 Base& Base::operator=(const Base &src) {
-    printf("Base: %p = %p \r\n", this, &src);
+    printf("Base: %p = %p \r\n", (void *)this, (void *)&src);
     return *this;
 }
 
 Base& Base::operator=(const Base &&src) {
-    printf("Base: move %p = %p \r\n", this, &src);
+    printf("Base: move %p = %p \r\n", (void *)this, (void *)&src);
     return *this;
 }
 
