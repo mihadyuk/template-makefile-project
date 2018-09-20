4b105948095f384a00c547f11cdf7d3dafc0257a
Andrey Mihadyuk <maoi.by@gmail.com>
2018-09-20 16:17:34 +0300

    Integrate template example

--- templates/templatesMain.cpp
+++ templates/templatesMain.cpp
@@ -4,8 +4,11 @@
  *  Created on: Sep 20, 2018
  *      Author: amihadyuk
  */
-
+#include <stdint.h>
+#include "templateExample.h"
 #include "templatesMain.h"
+
+Array<uint32_t, 10> g_array;
 
 void templatesMain() {
 
