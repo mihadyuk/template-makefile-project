298456cda513280a23a7c80b02332331963e3dc5
Andrey Mihadyuk <maoi.by@gmail.com>
2019-10-24 17:09:37 +0300

    smart pointers: update

--- smart_pointers/SmartPtr.h
+++ smart_pointers/SmartPtr.h
@@ -15,39 +15,61 @@ namespace smart_pointers {
 template <typename T>
 class SmartPtr {
 public:
-    SmartPtr() = default;
+    SmartPtr();
     SmartPtr(T *p);
-    SmartPtr(const SmartPtr<T> &src) = delete;
+    SmartPtr(const SmartPtr<T> &src);
     SmartPtr(SmartPtr<T> &&src)      = delete;
     SmartPtr<T>& operator=(const SmartPtr<T> &src) = delete;
     SmartPtr<T>& operator=(SmartPtr<T> &&src)      = delete;
     virtual ~SmartPtr();
 
+    uint32_t refCnt() const;
 private:
-    T *m_p             = nullptr;
-    uint32_t *m_refCnt = nullptr;
+    T *m_p;
+    uint32_t *m_refCnt;
 };
 
 template <typename T>
-SmartPtr<T>::SmartPtr(T *p) : m_p(p) {
+SmartPtr<T>::SmartPtr() : m_p(nullptr), m_refCnt(nullptr) {}
+
+template <typename T>
+SmartPtr<T>::SmartPtr(T *p) : m_p(p), m_refCnt(nullptr) {
+
     if (m_p) {
-        assert(m_refCnt == nullptr);
         m_refCnt = new uint32_t(1);
         assert(*m_refCnt == 1);
     }
 }
 
 template <typename T>
+SmartPtr<T>::SmartPtr(const SmartPtr<T> &src) : m_p(src.m_p), m_refCnt(src.m_refCnt) {
+    if (m_refCnt)
+        *m_refCnt += 1;
+}
+
+template <typename T>
 SmartPtr<T>::~SmartPtr() {
     if (m_refCnt) {
         (*m_refCnt)--;
-        if (*m_refCnt == 0 && m_p) {
-            delete m_p;
+        if (*m_refCnt == 0) {
+
             delete m_refCnt;
-            m_p = nullptr;
             m_refCnt = nullptr;
+
+            if (m_p) {
+                delete m_p;
+                m_p = nullptr;
+            }
         }
     }
+}
+
+template <typename T>
+uint32_t SmartPtr<T>::refCnt() const {
+    uint32_t retval = 0;
+    if (m_refCnt)
+        retval = *m_refCnt;
+    return retval;
 }
 
 }
--- smart_pointers/smartPtrsMain.cpp
+++ smart_pointers/smartPtrsMain.cpp
@@ -4,6 +4,7 @@
  *  Created on: Oct 24, 2019
  *      Author: amihadyuk
  */
+#include <stdio.h>
 
 #include "smartPtrsMain.h"
 #include "SmartPtr.h"
@@ -13,4 +14,10 @@ using namespace smart_pointers;
 void smartPtrsMain() {
 
     SmartPtr<uint32_t> ptr(new uint32_t(10));
+    //SmartPtr<uint32_t> ptr;
+    printf("refCnt: %d \r\n", ptr.refCnt());
+    SmartPtr<uint32_t> ptr2(ptr);
+    SmartPtr<uint32_t> ptr3;
+    //ptr3 = ptr2;
+    printf("refCnt: %d \r\n", ptr.refCnt());
 }
