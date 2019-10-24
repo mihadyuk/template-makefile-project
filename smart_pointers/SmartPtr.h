92d8cbee0dce9d8b9bb19c4d9ff8186059fcfa37
Andrey Mihadyuk <maoi.by@gmail.com>
2019-10-24 16:15:23 +0300

    Add smart pointer class to be implemented

---
+++ smart_pointers/SmartPtr.h
@@ -0,0 +1,21 @@
+/*
+ * SmartPtr.h
+ *
+ *  Created on: Oct 24, 2019
+ *      Author: amihadyuk
+ */
+
+#ifndef SMART_POINTERS_SMARTPTR_H_
+#define SMART_POINTERS_SMARTPTR_H_
+
+namespace smart_pointers {
+
+class SmartPtr {
+public:
+    SmartPtr();
+    virtual ~SmartPtr();
+};
+
+}
+
+#endif /* SMART_POINTERS_SMARTPTR_H_ */
