7b5daab9dd8af78b9a0a17a46a5b8974d94e4594
Andrey Mihadyuk <maoi.by@gmail.com>
2020-03-02 17:46:54 +0300

    Update comments

--- alt_stitcher.cpp
+++ alt_stitcher.cpp
@@ -6,7 +6,10 @@
  */
 #include "alt_stitcher.h"
 
-
+/**
+ * https://www.pyimagesearch.com/2016/01/25/real-time-panorama-and-image-stitching-with-opencv/
+ * https://www.pyimagesearch.com/2016/01/11/opencv-panorama-stitching/
+ * */
 int alt_stitcher(int argc, char* argv[]) {
     return 0;
 }
