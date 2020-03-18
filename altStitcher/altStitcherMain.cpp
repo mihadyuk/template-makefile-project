3991e1dbbbcaa6746e6eb9c1ebb2a87d2a9cc197
Andrey Mihadyuk <maoi.by@gmail.com>
2020-03-18 16:00:14 +0300

    Add alt stitcher

--- altStitcher/altStitcherMain.cpp
+++ altStitcher/altStitcherMain.cpp
@@ -1,7 +1,109 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/stitching.hpp>
+#include <iostream>
+#include <chrono>
+#include <stdlib.h>
+
 #include "altStitcher.h"
+
+using namespace std;
+using namespace cv;
+
+static bool divide_images = false;
+static Stitcher::Mode mode = Stitcher::PANORAMA;
+static vector<Mat> imgs;
+static string result_name = "result.jpg";
+
+static void printUsage(char** argv);
+static int parseCmdArgs(int argc, char** argv);
 
 
 int altStitcherMain(int argc, char* argv[])
 {
-    return 0;
+    int retval = parseCmdArgs(argc, argv);
+    if (retval)
+        return EXIT_FAILURE;
+
+    AltStitcher stitcher;
+    stitcher.stitch(imgs[0], imgs[1]);
+
+    return EXIT_SUCCESS;
 }
+
+static void printUsage(char** argv)
+{
+    cout <<
+         "Images stitcher.\n\n" << "Usage :\n" << argv[0] <<" [Flags] img1 img2 [...imgN]\n\n"
+         "Flags:\n"
+         "  --d3\n"
+         "      internally creates three chunks of each image to increase stitching success\n"
+         "  --mode (panorama|scans)\n"
+         "      Determines configuration of stitcher. The default is 'panorama',\n"
+         "      mode suitable for creating photo panoramas. Option 'scans' is suitable\n"
+         "      for stitching materials under affine transformation, such as scans.\n"
+         "  --output <result_img>\n"
+         "      The default is 'result.jpg'.\n\n"
+         "Example usage :\n" << argv[0] << " --d3 --try_use_gpu yes --mode scans img1.jpg img2.jpg\n";
+}
+
+static int parseCmdArgs(int argc, char** argv)
+{
+    if (argc == 1)
+    {
+        printUsage(argv);
+        return EXIT_FAILURE;
+    }
+    for (int i = 1; i < argc; ++i)
+    {
+        if (string(argv[i]) == "--help" || string(argv[i]) == "/?")
+        {
+            printUsage(argv);
+            return EXIT_FAILURE;
+        }
+        else if (string(argv[i]) == "--d3")
+        {
+            divide_images = true;
+        }
+        else if (string(argv[i]) == "--output")
+        {
+            result_name = argv[i + 1];
+            i++;
+        }
+        else if (string(argv[i]) == "--mode")
+        {
+            if (string(argv[i + 1]) == "panorama")
+                mode = Stitcher::PANORAMA;
+            else if (string(argv[i + 1]) == "scans")
+                mode = Stitcher::SCANS;
+            else
+            {
+                cout << "Bad --mode flag value\n";
+                return EXIT_FAILURE;
+            }
+            i++;
+        }
+        else
+        {
+            Mat img = imread(samples::findFile(argv[i]));
+            if (img.empty())
+            {
+                cout << "Can't read image '" << argv[i] << "'\n";
+                return EXIT_FAILURE;
+            }
+            if (divide_images)
+            {
+                Rect rect(0, 0, img.cols / 2, img.rows);
+                imgs.push_back(img(rect).clone());
+                rect.x = img.cols / 3;
+                imgs.push_back(img(rect).clone());
+                rect.x = img.cols / 2;
+                imgs.push_back(img(rect).clone());
+            }
+            else
+                imgs.push_back(img);
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
