4cfee6b6cbce5971ab71f512da258b1db5ee0ef6
Andrey Mihadyuk <maoi.by@gmail.com>
2021-03-22 18:56:04 +0300

    Add test code

--- main.cc
+++ main.cc
@@ -1,10 +1,15 @@
 #include <assert.h>
 #include <iostream>
+#include "settings.h"
 
+using namespace verifone::service::usbcomm;
 
 int main(int argc, char *argv[]) {
 
   std::cout << "template makefile project \r\n";
+
+  Settings &settings = Settings::instance();
+  settings.open("test.xml");
   return 0;
 }
 
