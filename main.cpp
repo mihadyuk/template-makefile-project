086c06c1668b14852c09fcc97228f47a4e7b15d7
Andrey Mihadyuk <maoi.by@gmail.com>
2021-04-23 23:22:11 +0300

    Add a new thread example

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 #include <future>
 #include <mutex>
 #include <condition_variable>
+#include <atomic>
 
 static std::mutex g_mutex;
 static std::condition_variable g_cv;
@@ -33,6 +34,22 @@ static int thread_func(void *p)
     return 1;
 }
 
+static std::atomic<bool> g_shouldClose(false);
+static std::mutex g_mutex_2;
+static void thread_func_2(void *p)
+{
+    while (1)
+    {
+        printf("wait for unlock\n");
+        g_mutex_2.lock();
+        printf("unlocked in thread\n");
+        if (g_shouldClose)
+        {
+            break;
+        }
+    }
+    printf("thread closed\n");
+}
 
 
 int main(int argc, char *argv[]) {
@@ -55,6 +72,17 @@ int main(int argc, char *argv[]) {
   //assert(status == std::cv_status::no_timeout);
   //std::future<int> result = std::async(std::launch::async, thread_func, nullptr);
   //auto result = std::async(std::launch::async, thread_func, nullptr);
+  g_mutex_2.lock();
+  std::thread thread_2(thread_func_2, nullptr);
+  std::this_thread::sleep_for(std::chrono::milliseconds(200));
+  g_mutex_2.unlock();
+  g_mutex_2.unlock();
+  std::this_thread::sleep_for(std::chrono::milliseconds(200));
+  g_mutex_2.unlock();
+  g_shouldClose = true;
+  g_mutex_2.unlock();
+  thread_2.join();
+
   printf("max timeout %llu\n", std::chrono::milliseconds::max().count());
   g_timestamp = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now());
   printf("start thread %u\n", (uint32_t)(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - g_timestamp)).count());
@@ -65,6 +93,7 @@ int main(int argc, char *argv[]) {
   printf("notification %u\n", (uint32_t)(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - g_timestamp)).count());
   g_cv.notify_one();
   thread.join();
+
   return 0;
 }
 
