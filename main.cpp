18bcf06b99a5be5e5658963da7e06dd2ecce00b3
Andrey Mihadyuk <maoi.by@gmail.com>
2022-07-21 11:03:13 +0300

    Add reading vid & pid

--- main.cpp
+++ main.cpp
@@ -4,13 +4,24 @@
 #include <memory>
 #include <stdio.h>
 
+#include <fstream>
 #include <vector>
 #include <unistd.h>
 #include <dirent.h>
 #include <string.h>
 
+static std::string getVidPid(const std::string &busInfo) {
 
-std::vector<std::string> listDir(const std::string &dir) {
+    std::string modaliasPath = "/sys/bus/usb/devices/" + busInfo + "/modalias";
+
+    std::ifstream modalias(modaliasPath, std::ios::in);
+    std::string retval;
+    modalias >> retval;
+    printf("modalias: \"%s\"", retval.c_str());
+    return retval;
+}
+
+static std::vector<std::string> listDir(const std::string &dir) {
     std::vector<std::string> data;
     struct dirent *dirent;
     char buffer[4096];
@@ -20,7 +31,7 @@ std::vector<std::string> listDir(const std::string &dir) {
     if (d) {
         while ((dirent = readdir(d)) != nullptr) {
             if (dirent->d_type == DT_LNK) {
-                memset(buffer, 0, sizeof(buffer));
+                memset(buffer, 0, sizeof(buffer)); // readlink does not paste "\n", so we have to clear buffer
                 ssize_t lnkSize = readlink((dir + "/" + dirent->d_name).c_str(), buffer, sizeof(buffer));
                 if (lnkSize > 0) {
                     //extract bus info
@@ -29,14 +40,18 @@ std::vector<std::string> listDir(const std::string &dir) {
                     if (posDev != std::string::npos) {
                         std::string::size_type posDevBus = lnk.rfind("/", posDev - 1);
                         if (posDevBus != std::string::npos) {
-                            data.push_back(lnk.substr(posDevBus + 1, posDev - posDevBus - 1));
+                            std::string busInfo(lnk.substr(posDevBus + 1, posDev - posDevBus - 1));
+
+                            // get vid & pid using bus info
+                            data.push_back(std::string(dirent->d_name) + " " + busInfo + " " + getVidPid(busInfo));
                         }
                     }
                 }
             }
         }
+        closedir(d);
     }
-    closedir(d);
+
     return data;
 }
 
