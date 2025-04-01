10f19b55ec0105475a7e7bd69614839c9159f8bb
Andrey Mihadyuk <maoi.by@gmail.com>
2025-04-01 15:11:15 +0300

    fork: add basic functionality

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <assert.h>
 #include <iostream>
+#include <stdio.h>
+
+#include <unistd.h>
 
 
 int main(int argc, char *argv[]) {
 
-  std::cout << "template makefile project \r\n";
+  //std::cout << "template makefile project \r\n";
+  printf("before fork\n");
+  pid_t pid = fork();
+  if (pid == 0) {
+    printf("child proc. proc id 0x%.8X\n", getpid());
+  }
+  else if (pid > 0) {
+    printf("parent proc. child proc id 0x%.8X, parent proc id 0x%.8X\n", pid, getpid());
+  }
+  else {
+    printf("fork() failed. return code %d", pid);
+  }
   return 0;
 }
 
