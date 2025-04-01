c3d1c4250f0c7e8e16a7b845e4b5075966490e9b
Andrey Mihadyuk <maoi.by@gmail.com>
2025-04-01 15:22:01 +0300

    fork: add exit code

--- main.cpp
+++ main.cpp
@@ -12,9 +12,11 @@ int main(int argc, char *argv[]) {
   pid_t pid = fork();
   if (pid == 0) {
     printf("child proc. proc id 0x%.8X\n", getpid());
+    _exit(EXIT_SUCCESS);
   }
   else if (pid > 0) {
     printf("parent proc. child proc id 0x%.8X, parent proc id 0x%.8X\n", pid, getpid());
+    exit(EXIT_SUCCESS);
   }
   else {
     printf("fork() failed. return code %d", pid);
