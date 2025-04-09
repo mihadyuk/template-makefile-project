84b5f63a297b96de39db52b82b4836ba488135f7
andrey mihadyuk <maoi.by@gmail.com>
2025-04-09 19:27:33 +0300

    qt: add some test code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <assert.h>
 #include <iostream>
-
+#include <stdio.h>
+#include <bitset>
 
 int main(int argc, char *argv[]) {
 
-  std::cout << "template makefile project \r\n";
-  return 0;
+    std::cout << "template makefile project \r\n";
+    for (int i = 0; i < argc; i++) {
+        printf("arg[%d] : \"%s\"\r\n", i, argv[i]);
+    }
+
+    std::bitset<8> bits(0x5);
+    bits[5] = 1;
+    auto str = bits.to_string();
+    printf("size %llu, bits : %s\r\n", sizeof(bits), str.c_str());
+    return 0;
 }
 
 
