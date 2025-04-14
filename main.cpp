45abd744290a2b32e13fab0bf93582706678552c
andrey mihadyuk <maoi.by@gmail.com>
2025-04-14 22:00:50 +0300

    qt: two sum task

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <bitset>
 #include <stdint.h>
 #include <vector>
+#include <unordered_map>
 //#include "qt_subdir/qtexampleskeleton.h"
 
 //reverse int, example 321 -> 123
@@ -52,12 +53,62 @@ int16_t reverse(int16_t val) {
     return val >= 0 ? retval : -retval;
 }
 
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+        You can return the answer in any order.
+
+
+
+        Example 1:
+
+                    Input: nums = [2,7,11,15], target = 9
+                 Output: [0,1]
+                          Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+    Example 2:
+
+                Input: nums = [3,2,4], target = 6
+    Output: [1,2]
+             Example 3:
+
+                         Input: nums = [3,3], target = 6
+             Output: [0,1]
+
+
+                      Constraints:
+
+                                    2 <= nums.length <= 104
+                                           -109 <= nums[i] <= 109
+                                           -109 <= target <= 109
+                                       Only one valid answer exists.
+*/
+std::vector<int> twoSum(const std::vector<int> &data, int target) {
+    std::unordered_map<int, int> map;
+
+    for (int i = 0; i < static_cast<int>(data.size()); i++) {
+        int sup = target - data[i];
+        if (map.contains(data[i]) == false) {
+            map[sup] = i;
+        }
+        else {
+            return {i, map[data[i]]};
+        }
+    }
+    return std::vector<int>();
+}
+
 int main(int argc, char *argv[]) {
 
     std::cout << "template makefile project \r\n";
     for (int i = 0; i < argc; i++) {
         printf("arg[%d] : \"%s\"\r\n", i, argv[i]);
     }
+
+    auto vec = twoSum({2, 7, 11, 15}, 9);
+    vec = twoSum({3, 2, 4}, 6);
+
     std::bitset<8> bits(0x5);
     bits[5] = 1;
     auto str = bits.to_string();
