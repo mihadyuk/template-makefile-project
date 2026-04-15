ad0af58e658b674a5e933eabeb5c24117acb403e
andrey mihadyuk <maoi.by@gmail.com>
2026-04-15 20:07:22 +0300

    Make TimeElapsed as template

--- utilits/timeElapsed.h
+++ utilits/timeElapsed.h
@@ -8,15 +8,32 @@
 #ifndef TIMEELAPSED_H_
 #define TIMEELAPSED_H_
 #include <chrono>
+#include <type_traits>
 
-class TimeElapsed {
+template <typename ClockSource>
+class TimeElapsedBase {
+#if 0
+    static_assert(std::is_same_v<ClockSource, std::chrono::steady_clock> ||
+                  std::is_same_v<ClockSource, std::chrono::high_resolution_clock>);
+    static_assert(ClockSource::is_steady);
+#endif
+    ClockSource::time_point timeStamp_;
 public:
-  TimeElapsed();
-  virtual ~TimeElapsed();
-  void start();
-  std::chrono::milliseconds elapsedMs();
-private:
-  std::chrono::steady_clock::time_point timeStamp_;
+    TimeElapsedBase() {
+        start();
+    }
+    virtual ~TimeElapsedBase() = default;
+
+    void start() {
+        timeStamp_ = ClockSource::now();
+    }
+
+    std::chrono::milliseconds elapsedMs() {
+        return std::chrono::duration_cast<std::chrono::milliseconds>(ClockSource::now() - timeStamp_);
+    }
 };
+
+using TimeElapsed = TimeElapsedBase<std::chrono::steady_clock>;
+
 
 #endif //TIMEELAPSED_H_
